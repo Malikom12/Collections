1143cef5d510fec77da0d8ee26456d9cd0db6502
muham <muhammadmalikomer@gmail.com>
2024-01-24 11:57:39 +0000

    Added a way to increase capacity of arraylist Added validation

--- src/main/java/org/example/ArrayListEx.java
+++ src/main/java/org/example/ArrayListEx.java
@@ -1,28 +1,59 @@
 package org.example;
 
 public class ArrayListEx {
+    private static final int MAX_EXPANSION = 10000;
+    private static final int MAX_INITIAL_CAPACITY = 10000;
     private int[] data;
-    private int num_elements;
+    private int numElements;
+    private int capacity = 10;
+    private int expansionFactor = 10;
+
+    public void MyArrayList(int capacity){
+        this.capacity = capacity;
+        if(capacity <= MAX_INITIAL_CAPACITY && capacity > 0){
+            this.capacity = capacity;
+        }else if (capacity > MAX_INITIAL_CAPACITY){
+            this.capacity = MAX_INITIAL_CAPACITY;
+        }
+        data = new int[capacity];
+    }
+
+    public void MyArrayList(int capacity, int expansionFactor){
+        this.capacity = capacity;
+        if(capacity <= MAX_INITIAL_CAPACITY && capacity > 0){
+            this.capacity = capacity;
+        }else if (capacity > MAX_INITIAL_CAPACITY){
+            this.capacity = MAX_INITIAL_CAPACITY;
+        }
+
+        if(expansionFactor <= MAX_EXPANSION && expansionFactor > 0){
+            this.expansionFactor = expansionFactor;
+        }else if (expansionFactor > MAX_EXPANSION){
+            this.expansionFactor = MAX_EXPANSION;
+        }
+        data = new int[capacity];
+    }
 
     public void MyArrayList(){
-        data = new int[10];
+        capacity = 10;
+        data = new int[capacity];
     }
 
     public int size(){
-        return num_elements;
+        return numElements;
     }
 
     public void add(int num){
-        if (num_elements == data.length){
+        if (numElements == data.length){
             grow();
         }
-        data[num_elements] = num;
-        num_elements++;
+        data[numElements] = num;
+        numElements++;
     }
 
     private void grow(){
         int[] newData = new int[data.length + 10];
-        for (int i = 0; i < num_elements; i++){
+        for (int i = 0; i < numElements; i++){
             newData[i] = data[i];
         }
         data = newData;
